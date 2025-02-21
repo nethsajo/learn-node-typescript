41a8175613c63cc9c403eefb87ca2457bd972570
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2025-02-21 14:30:34 +0800

    add product type and update templates

---
+++ 1. Node Farm/src/types/product.ts
@@ -0,0 +1,11 @@
+export type Product = {
+  id: number;
+  productName: string;
+  image: string;
+  from: string;
+  nutrients: string;
+  quantity: number;
+  price: number;
+  organic: boolean;
+  description: string;
+};
