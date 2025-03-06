c6930a7684516c67a0e816a7687be101d868329f
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2025-03-06 22:39:48 +0800

    update natours app

--- Natours/src/app.ts
+++ Natours/src/app.ts
@@ -1,15 +1,45 @@
 import express, { Request, Response } from 'express';
+import fs from 'fs';
 import { StatusCodes } from 'http-status-codes';
 import { APP_PORT } from './env';
+import { Tour } from './types/tour';
 
 const app = express();
 
-app.get('/', (request: Request, response: Response) => {
-  response.status(StatusCodes.OK).json({ app: 'Natours', message: 'Hello from Natours API!' });
+/***
+ * Middleware
+ * Basically a function that can modify the incoming request data
+ * It's just a step that the request goes through while it's being processed
+ *  */
+
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/data/tours-simple.json`, 'utf-8')) as Tour[];
+
+app.get('/api/v1/tours', (request: Request, response: Response) => {
+  response.status(StatusCodes.OK).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post('/', (request: Request, response: Response) => {
-  response.send('You can post to this endpoint...');
+app.post('/api/v1/tours', (request: Request, response: Response) => {
+  const newId = tours[tours.length - 1]?.id ?? 0 + 1;
+  const newTour = Object.assign({ id: newId }, request.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(`${__dirname}/data/tours-simple.json`, JSON.stringify(tours), error => {
+    response.status(StatusCodes.CREATED).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  });
 });
 
 app.listen(APP_PORT, () => {
