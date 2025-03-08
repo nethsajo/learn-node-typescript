f63f92f4e538a6b455a072a4594a5cd112bf1369
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2025-03-09 00:21:24 +0800

    add morgan and user api

--- Natours/src/app.ts
+++ Natours/src/app.ts
@@ -1,9 +1,14 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import fs from 'fs';
 import { StatusCodes } from 'http-status-codes';
+import morgan from 'morgan';
 import { z } from 'zod';
 import { envConfig } from './env';
 import { Tour, tourSchemaObject } from './types/tour';
+
+const app = express();
+app.use(express.json());
+app.use(morgan('tiny'));
 
 /***
  * Middleware
@@ -11,14 +16,23 @@ import { Tour, tourSchemaObject } from './types/tour';
  * It's just a step that the request goes through while it's being processed
  *  */
 
-const app = express();
-app.use(express.json());
+app.use((request: Request, response: Response, next: NextFunction) => {
+  console.log('Hello from the middleware');
+  // If the next didn't call here then the request/response cycle would stuck and never send back a response to the client
+  next();
+});
+
+app.use((request: Request & { requestTime?: string }, response: Response, next: NextFunction) => {
+  request.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/mock/tours-simple.json`, 'utf-8')) as Tour[];
 
-const getTours = (request: Request, response: Response) => {
+const getTours = (request: Request & { requestTime?: string }, response: Response) => {
   response.status(StatusCodes.OK).json({
     status: 'success',
+    requested_at: request.requestTime,
     results: tours.length,
     data: {
       tours,
@@ -128,5 +142,43 @@ const deleteTour = (request: Request, response: Response) => {
 
 app.route('/api/v1/tours').get(getTours).post(createTour);
 app.route('/api/v1/tours/:id').get(getTour).put(updateTour).delete(deleteTour);
+
+const getUsers = (request: Request, response: Response) => {
+  response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const createUser = (request: Request, response: Response) => {
+  response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const getUser = (request: Request, response: Response) => {
+  response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const updateUser = (request: Request, response: Response) => {
+  response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const deleteUser = (request: Request, response: Response) => {
+  response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+app.route('/api/v1/users').get(getUsers).post(createUser);
+app.route('/api/v1/users/:id').get(getUser).put(updateUser).delete(deleteUser);
 
 app.listen(envConfig.APP_PORT, () => console.log('Listening on port', envConfig.APP_PORT));
