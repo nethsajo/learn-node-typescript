3edbf2bf951de3dafa06b9a0db58c4384d19208b
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2025-03-21 22:54:08 +0800

    update app

--- Natours/src/app.ts
+++ Natours/src/app.ts
@@ -1,18 +1,11 @@
-import express, { type NextFunction, type Request, type Response } from 'express';
+import express from 'express';
 import morgan from 'morgan';
 import { routes } from './controllers/routes';
-import { createDbClient } from './db/create-db-client';
 import { envConfig } from './env';
-
-const dbClient = createDbClient();
 
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
-app.use((request: Request, response: Response, next: NextFunction) => {
-  response.locals.dbClient = dbClient;
-  next();
-});
 
 /* Routes */
 routes.forEach(route => {
