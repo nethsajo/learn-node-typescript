a40ec4764275fa7ee3c5fbfe148516dfa6bdad7c
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2025-03-22 23:41:08 +0800

    update db schema

--- Natours/src/db/schema.ts
+++ Natours/src/db/schema.ts
@@ -1,3 +1,15 @@
-import { type DB } from './types';
+import { type DB, type users } from './types';
+
+type OverrideCommonFields<Model> = Omit<
+  Model,
+  'id' | 'created_at' | 'updated_at' | 'deleted_at'
+> & {
+  id: number;
+  created_at: Date | string;
+  updated_at: Date | string;
+  deleted_at: Date | string | null;
+};
+
+export type User = OverrideCommonFields<users>;
 
 export type KyselySchema = DB;
