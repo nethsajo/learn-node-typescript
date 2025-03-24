ffccb312bc3606bdea9699ca19f8de1894f46d66
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2025-03-24 22:35:55 +0800

    update user routes

--- Natours/src/controllers/users/routes.ts
+++ Natours/src/controllers/users/routes.ts
@@ -1,10 +1,11 @@
 import express, { type Router } from 'express';
-import { archiveUser, createUser, getUser, updateUser } from './controller';
+import { archiveUser, getUser, updateUser } from './controller';
+import { createUserRouteHandler } from './create-user';
 import { getUsersRouteHandler } from './get-users';
 
 const usersRoutes: Router = express.Router();
 
-usersRoutes.route('/users').get(getUsersRouteHandler).post(createUser);
+usersRoutes.route('/users').get(getUsersRouteHandler).post(createUserRouteHandler);
 usersRoutes.route('/users/:id').get(getUser).put(updateUser).delete(archiveUser);
 
 export default usersRoutes;
