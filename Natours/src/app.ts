3c107d7f9689b5ea04c2a5e99ff4e66e65b65513
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2025-03-25 22:06:24 +0800

    update natours app

--- Natours/src/app.ts
+++ Natours/src/app.ts
@@ -1,15 +1,21 @@
+import { routes } from '@/controllers/routes';
+import { schemas } from '@/data/schemas';
+import { errorHandlerMiddleware } from '@/middlewares/error-handler';
+import { registry } from '@/utils/registry';
 import { OpenApiGeneratorV3 } from '@asteasolutions/zod-to-openapi';
+import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
 import swaggerUi from 'swagger-ui-express';
 import { version } from '../package.json';
-import { routes } from './controllers/routes';
-import { schemas } from './data/schemas';
 import { envConfig } from './env';
-import { errorHandlerMiddleware } from './middlewares/error-handler';
-import { registry } from './utils/registry';
 
 const app = express();
+
+/* Register Schemas */
+Object.entries(schemas).forEach(([key, value]) => {
+  registry.register(key, value);
+});
 
 app.use(
   '/swagger',
@@ -26,23 +32,24 @@ app.use(
         description: 'API Reference',
         url: '/reference',
       },
-    })
+    }),
+    { customCss: '.topbar { display: none; }' }
   )
 );
 
-/* Register Schemas */
-Object.entries(schemas).forEach(([key, value]) => {
-  registry.register(key, value);
-});
-
-app.use(errorHandlerMiddleware);
-app.use(express.json());
+/* Middleware */
+app.use(cors());
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 /* Routes */
 routes.forEach(route => {
   app.use('/api/v1', route);
 });
+
+/* Error handling middleware */
+app.use(errorHandlerMiddleware);
 
 /* Server */
 app.listen(envConfig.APP_PORT, () => {
--- Natours/src/middlewares/setup-db-client.ts
+++ Natours/src/middlewares/setup-db-client.ts
@@ -1,1 +1,10 @@
-.gitkeep
+import { createDbClient } from '@/db/create-db-client';
+import { type NextFunction, type Request, type Response } from 'express';
+
+const dbClient = createDbClient();
+
+export async function setupDbClientMiddleware(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {}
