fed3fa308aa60038ca6c96d375ec67490d33b0db
Jan Kenneth Sajo <nethsajo98@gmail.com>
2025-03-26 15:03:23 +0800

    update registry

--- Natours/src/utils/registry.ts
+++ Natours/src/utils/registry.ts
@@ -1,3 +1,22 @@
-import { OpenAPIRegistry } from '@asteasolutions/zod-to-openapi';
+import { envConfig } from '@/env';
+import { OpenApiGeneratorV3, OpenAPIRegistry } from '@asteasolutions/zod-to-openapi';
+import { version } from '../../package.json';
+import { type OpenAPIDefinitions } from '@asteasolutions/zod-to-openapi/dist/openapi-registry';
 
 export const registry = new OpenAPIRegistry();
+
+export const generateOpenAPISpec = (definitions: OpenAPIDefinitions[]) => {
+  const generator = new OpenApiGeneratorV3(definitions);
+  return generator.generateDocument({
+    openapi: '3.0.0',
+    info: {
+      version,
+      title: `${envConfig.STAGE.toUpperCase()} API`,
+      description: 'API Documentation',
+    },
+    externalDocs: {
+      description: 'API Reference',
+      url: '/reference',
+    },
+  });
+};
