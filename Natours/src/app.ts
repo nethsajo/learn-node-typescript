298b346fa9d8045d74781c7711dc722fad6653a8
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2025-04-06 22:10:58 +0800

    add db client middleware

--- Natours/src/app.ts
+++ Natours/src/app.ts
@@ -43,6 +43,7 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(setupDbClientMiddleware);
 
 /* Routes */
 routes.forEach(route => {
--- Natours/src/middlewares/setup-db-client.ts
+++ Natours/src/middlewares/setup-db-client.ts
@@ -4,8 +4,6 @@ import { createDbClient } from '@/db/create-db-client';
 
 const dbClient = createDbClient();
 
-export async function setupDbClientMiddleware(
-  request: Request,
-  response: Response,
-  next: NextFunction
-) {}
+export async function setupDbClientMiddleware(request: Request, response: Response, next: NextFunction) {
+  next();
+}
