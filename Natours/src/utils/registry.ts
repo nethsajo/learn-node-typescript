41b9118f18fa6a7019a4b664930704e29b6db414
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2025-04-11 19:13:57 +0800

    update registry

--- Natours/src/utils/registry.ts
+++ Natours/src/utils/registry.ts
@@ -1,4 +1,4 @@
-import { OpenApiGeneratorV3,OpenAPIRegistry } from '@asteasolutions/zod-to-openapi';
+import { OpenApiGeneratorV3, OpenAPIRegistry } from '@asteasolutions/zod-to-openapi';
 import { type OpenAPIDefinitions } from '@asteasolutions/zod-to-openapi/dist/openapi-registry';
 
 import { envConfig } from '@/env';
@@ -6,6 +6,12 @@ import { envConfig } from '@/env';
 import { version } from '../../package.json';
 
 export const registry = new OpenAPIRegistry();
+
+registry.registerComponent('securitySchemes', 'bearerAuth', {
+  type: 'http',
+  scheme: 'bearer',
+  bearerFormat: 'JWT',
+});
 
 export const generateOpenAPISpec = (definitions: OpenAPIDefinitions[]) => {
   const generator = new OpenApiGeneratorV3(definitions);
