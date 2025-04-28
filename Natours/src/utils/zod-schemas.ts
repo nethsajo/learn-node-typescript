f71463780902571e0022bdf62e765743b77db136
Jan Kenneth Sajo <nethsajo98@gmail.com>
2025-04-28 08:33:23 +0800

    add email and password zod schema

--- Natours/src/utils/zod-schemas.ts
+++ Natours/src/utils/zod-schemas.ts
@@ -1,5 +1,34 @@
 import { z } from 'zod';
 
+export const emailSchema = z.string().email('Please enter a valid email address').toLowerCase();
+/**  Add refine if email has own domain
+ * export const emailSchema = z.string()
+ * 	.email('Please enter a valid email address')
+ * 	.toLowerCase()
+ * 	.refine(value => value.endsWith('@sample.com'), {
+			message: 'Email must end with @sample.com',
+		});
+ * */
+
+const passwordValidationMessages = [
+  'Password must be at least 8 characters long',
+  'Password must have 1 lowercase',
+  'Password must have 1 uppercase ',
+  'Password must have 1 special character, and 1 number.',
+  'Password should not exceed 16 characters',
+];
+
+export const passwordSchema = z.string().refine(value => {
+  const minLength = value.length >= 8;
+  const maxLength = value.length <= 16;
+  const hasLowerCase = /[a-z]/.test(value);
+  const hasUpperCase = /[A-Z]/.test(value);
+  const hasNumber = /\d/.test(value);
+  const hasSpecialChar = /[^A-Za-z0-9]/.test(value);
+
+  return minLength && maxLength && hasLowerCase && hasUpperCase && hasNumber && hasSpecialChar;
+}, passwordValidationMessages.join('\n'));
+
 export const paginationSchema = z.object({
   total_pages: z.number(),
   current_page: z.number(),
